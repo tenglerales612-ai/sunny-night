f20ce1095df9b34d144312e0881bb8f118fb4448
tenglerales612-ai <tenglerales612@gmail.com>
2026-06-09 12:09:52 +0200

    Add files via upload

---
+++ !DOCTYPE  java.js
@@ -0,0 +1,15 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+reveals.forEach(section => {
+
+const top = section.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+section.classList.add("active");
+}
+
+});
+
+});
